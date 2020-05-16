c10ef14b1dfde2079c9b6193f7b468e111f8c58b
surendrak@ibaseit.com <surendrak@ibaseit.com>
2020-05-16 15:13:35 +0530

    intial commint

---
+++ server.js
@@ -0,0 +1,16 @@
+"use strict"
+
+module.exports = {
+
+    RandomChar: (num) => {
+        let string = "dsgbjkdfnnfkl47469058970850hdbgd546";
+        let str = '';
+        let i = 0;
+        while(i < num) {
+            str += string.charAt(Math.floor(Math.random() * string.length ));
+            i++;
+        }
+        return str;
+    }
+
+}
